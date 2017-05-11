d4f6176a7ba67c25496876d22a69c3cfdbaac42d
laba7 <oleglaba97@gmail.com>
2017-05-12 01:56:57 +0300

    Update SuccessAdd.java

--- src/test/java/com/work/test/SuccessAdd.java
+++ src/test/java/com/work/test/SuccessAdd.java
@@ -13,7 +13,7 @@ public class SuccessAdd {
 
     private WebDriver driver;
     private WebDriverWait wait;
-    private By profile = By.className("link inlblk");
+    private By profile = By.id("topLoginLink");
     private By ad = By.id("login-box-ads");
 
     public SuccessAdd(WebDriver driver) {
@@ -22,12 +22,13 @@ public class SuccessAdd {
     }
 
     public void controlAd(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("topLoginLink")));
         WebElement profile = driver.findElement(this.profile);
         WebElement ad = driver.findElement(this.ad);
-
-        Actions actions = new Actions(driver);
-        actions.moveToElement(profile)
-                .moveToElement(ad)
-                .click().build().perform();
+        profile.click();
+//        Actions actions = new Actions(driver);
+//        actions.moveToElement(profile)
+//                .moveToElement(ad)
+//                .click().build().perform();
     }
 }
